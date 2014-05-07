a52c9ae4fcba54f634924f0f9fdc05a0de774049
David Nolen <davidnolen@Davids-iMac.local>
2014-05-07 09:30:34 -0400

    more benchmarks

--- bench/bench.js
+++ bench/bench.js
@@ -1,14 +1,18 @@
+// Copyright (c) Cognitect, Inc.
+// All rights reserved.
+
 "use strict";
 
 var eq = require("../src/transit/eq.js"),
     t  = require("../src/transit/types.js");
 
 function time(f) {
-  for(var i = 0; i < 10; i++) {
+  //for(var i = 0; i < 10; i++) {
     var s = new Date();
     f();
     console.log("Elapsed "+((new Date()).valueOf()-s.valueOf())+"ms");
-  }
+  //}
+  console.log("----------");
 }
 
 console.log("1e6 iters, eq.equals(\"foo\", \"foo\")");
@@ -22,6 +26,51 @@ console.log("1e6 iters, eq.equals(\"foo\", \"bar\")");
 time(function() {
   for(var i = 0; i < 1000000; i++) {
     eq.equals("foo", "bar");
+  }
+});
+
+console.log("1e6 iters, eq.equals([1,2,3], [1,2,3])");
+time(function() {
+  var arr0 = [1,2,3],
+      arr1 = [1,2,3];
+  for(var i = 0; i < 1000000; i++) {
+    eq.equals(arr0, arr1);
+  }
+});
+
+console.log("1e6 iters, eq.equals([1,2,3], [3,2,3])");
+time(function() {
+  var arr0 = [1,2,3],
+      arr1 = [3,2,3];
+  for(var i = 0; i < 1000000; i++) {
+    eq.equals(arr0, arr1);
+  }
+});
+
+console.log("1e6 iters, eq.equals(new Keyword(\"foo\"), new Keyword(\"foo\"))");
+time(function() {
+  var k0 = t.Keyword("foo"),
+      k1 = t.Keyword("foo");
+  for(var i = 0; i < 1000000; i++) {
+    eq.equals(k0, k1);
+  }
+});
+
+console.log("1e6 iters, eq.equals(new Keyword(\"foo\"), new Keyword(\"bar\"))");
+time(function() {
+  var k0 = t.Keyword("foo"),
+      k1 = t.Keyword("bar");
+  for(var i = 0; i < 1000000; i++) {
+    eq.equals(k0, k1);
+  }
+});
+
+console.log("1e6 iters, eq.equals({foo: \"bar\"}, {foo: \"bar\"})");
+time(function() {
+  var obj0 = {foo: "bar"},
+      obj1 = {foo: "bar"};
+  for(var i = 0; i < 1000000; i++) {
+    eq.equals(obj0, obj1);
   }
 });
 
