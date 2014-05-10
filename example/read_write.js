6cdb514966e02982f7b293aac6f978af41b67a8e
David Nolen <dnolen@cognitect.com>
2014-05-09 20:03:30 -0400

    wishful thinking

--- example/read_write.js
+++ example/read_write.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
 
-var transit = require("../src/transit.js");
+process.stdin.setEncoding("utf8");
 
+var transit   = require("../src/transit.js"),
+    transport = process.argv[0] || "json",
+    r         = transit.reader(transport),
+    w         = transit.writer(process.stdout);
+
+transit.read(process.stdin, function(data, err) {
+  transit.write(w, data);
+});
