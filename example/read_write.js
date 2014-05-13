7886519e5c8550d0b2514f59aa5c13741323405d
David Nolen <dnolen@cognitect.com>
2014-05-13 16:35:58 -0400

    tweak roundtripping script

--- example/read_write.js
+++ example/read_write.js
@@ -2,11 +2,13 @@
 
 process.stdin.setEncoding("utf8");
 
+console.log(process.argv[2]);
+
 var transit   = require("../src/transit.js"),
-    transport = process.argv[0] || "json",
+    transport = process.argv[2] || "json",
     r         = transit.reader(transport),
-    w         = transit.writer();
+    w         = transit.writer(transport);
 
 process.stdin.on("data", function(data, err) {
-    process.stdout.write(transit.write(transit.read(data)));
+    process.stdout.write(w.write(r.read(data)));
 });
