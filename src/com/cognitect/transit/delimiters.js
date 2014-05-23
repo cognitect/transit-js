b36a30ad2e36308d5ecda7bff003609dfbcc099c
David Nolen <david.nolen@gmail.com>
2014-05-23 08:11:50 -0400

    make delimiters @const

--- src/com/cognitect/transit/delimiters.js
+++ src/com/cognitect/transit/delimiters.js
@@ -9,10 +9,34 @@ goog.scope(function() {
 
 var delimiters = com.cognitect.transit.delimiters;
     
+/**
+ * @const
+ * @type {string}
+ */
 delimiters.ESC     = "~";
+
+/**
+ * @const
+ * @type {string}
+ */
 delimiters.TAG     = "#";
+
+/**
+ * @const
+ * @type {string}
+ */
 delimiters.SUB     = "^";
+
+/**
+ * @const
+ * @type {string}
+ */
 delimiters.RES     = "`";
+
+/**
+ * @const
+ * @type {string}
+ */
 delimiters.ESC_TAG = "~#";
 
 });
