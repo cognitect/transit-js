9fd38fc46624d3af2fd8df53f79830cb4b3d0e8d
David Nolen <david.nolen@gmail.com>
2014-06-28 09:42:39 +0200

    IE polyfill stuff

---
+++ src/com/cognitect/transit/util.js
@@ -0,0 +1,34 @@
+// Copyright (c) Cognitect, Inc.
+// All rights reserved.
+
+"use strict";
+
+goog.provide("com.cognitect.transit.util");
+goog.require("goog.object");
+
+goog.scope(function() {
+
+var util    = com.cognitect.transit.util,
+    gobject = goog.object;
+
+if(typeof Object.keys != undefined) {
+    util.objectKeys = function(obj) {
+        return Object.keys(obj);
+    };
+} else {
+    util.objectKeys = function(obj) {
+        return gobject.getKeys(obj);
+    };
+}
+
+if(typeof Array.isArray != undefined) {
+    util.isArray = function(obj) {
+        return Array.isArray(obj);
+    };
+} else {
+    util.isArray = function(obj) {
+        return goog.typeOf(obj) === "array";
+    };
+}
+
+});
