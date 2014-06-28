e0ccd306b40be232c71b62a232c7dc170c1dd414
David Nolen <david.nolen@gmail.com>
2014-06-28 11:53:53 +0200

    fix handlers namespace

--- src/com/cognitect/transit/handlers.js
+++ src/com/cognitect/transit/handlers.js
@@ -72,208 +72,208 @@ handlers.stringableKeys = function(m) {
 /**
  * @constructor
  */
-var NilHandler = function(){};
-NilHandler.prototype.tag = function(v) { return "_"; };
-NilHandler.prototype.rep = function(v) { return null; };
-NilHandler.prototype.stringRep = function(v) { return "null"; };
+handlers.NilHandler = function(){};
+handlers.NilHandler.prototype.tag = function(v) { return "_"; };
+handlers.NilHandler.prototype.rep = function(v) { return null; };
+handlers.NilHandler.prototype.stringRep = function(v) { return "null"; };
 
 /**
  * @constructor
  */
-var StringHandler = function(){};
-StringHandler.prototype.tag = function(v) { return "s"; };
-StringHandler.prototype.rep = function(v) { return v; };
-StringHandler.prototype.stringRep = function(v) { return v; };
+handlers.StringHandler = function(){};
+handlers.StringHandler.prototype.tag = function(v) { return "s"; };
+handlers.StringHandler.prototype.rep = function(v) { return v; };
+handlers.StringHandler.prototype.stringRep = function(v) { return v; };
 
 /**
  * @constructor
  */
-var NumberHandler = function(){};
-NumberHandler.prototype.tag = function(v) { return "i"; };
-NumberHandler.prototype.rep = function(v) { return v; };
-NumberHandler.prototype.stringRep = function(v) { return v.toString(); };
+handlers.NumberHandler = function(){};
+handlers.NumberHandler.prototype.tag = function(v) { return "i"; };
+handlers.NumberHandler.prototype.rep = function(v) { return v; };
+handlers.NumberHandler.prototype.stringRep = function(v) { return v.toString(); };
 
 /**
  * @constructor
  */
-var IntegerHandler = function(){};
-IntegerHandler.prototype.tag = function(v) { return "i"; };
-IntegerHandler.prototype.rep = function(v) { return v.toString(); };
-IntegerHandler.prototype.stringRep = function(v) { return v.toString(); };
+handlers.IntegerHandler = function(){};
+handlers.IntegerHandler.prototype.tag = function(v) { return "i"; };
+handlers.IntegerHandler.prototype.rep = function(v) { return v.toString(); };
+handlers.IntegerHandler.prototype.stringRep = function(v) { return v.toString(); };
 
 /**
  * @constructor
  */
-var BigIntegerHandler = function(){};
-BigIntegerHandler.prototype.tag = function(v) { return "n"; };
-BigIntegerHandler.prototype.rep = function(v) { return v.value; };
-BigIntegerHandler.prototype.stringRep = function(v) { return v.value; };
+handlers.BigIntegerHandler = function(){};
+handlers.BigIntegerHandler.prototype.tag = function(v) { return "n"; };
+handlers.BigIntegerHandler.prototype.rep = function(v) { return v.value; };
+handlers.BigIntegerHandler.prototype.stringRep = function(v) { return v.value; };
 
 /**
  * @constructor
  */
-var BigDecimalHandler = function(){};
-BigDecimalHandler.prototype.tag = function(v) { return "f"; };
-BigDecimalHandler.prototype.rep = function(v) { return v.value; };
-BigDecimalHandler.prototype.stringRep = function(v) { return v.value; };
+handlers.BigDecimalHandler = function(){};
+handlers.BigDecimalHandler.prototype.tag = function(v) { return "f"; };
+handlers.BigDecimalHandler.prototype.rep = function(v) { return v.value; };
+handlers.BigDecimalHandler.prototype.stringRep = function(v) { return v.value; };
 
 /**
  * @constructor
  */
-var BooleanHandler = function(){};
-BooleanHandler.prototype.tag = function(v) { return "?"; };
-BooleanHandler.prototype.rep = function(v) { return v; };
-BooleanHandler.prototype.stringRep = function(v) { return v.toString(); };
+handlers.BooleanHandler = function(){};
+handlers.BooleanHandler.prototype.tag = function(v) { return "?"; };
+handlers.BooleanHandler.prototype.rep = function(v) { return v; };
+handlers.BooleanHandler.prototype.stringRep = function(v) { return v.toString(); };
 
 /**
  * @constructor
  */
-var ArrayHandler = function(){};
-ArrayHandler.prototype.tag = function(v) { return "array"; };
-ArrayHandler.prototype.rep = function(v) { return v; };
-ArrayHandler.prototype.stringRep = function(v) { return null; };
+handlers.ArrayHandler = function(){};
+handlers.ArrayHandler.prototype.tag = function(v) { return "array"; };
+handlers.ArrayHandler.prototype.rep = function(v) { return v; };
+handlers.ArrayHandler.prototype.stringRep = function(v) { return null; };
 
 /**
  * @constructor
  */
-var MapHandler = function(){};
-MapHandler.prototype.tag = function(v) { return "map"; };
-MapHandler.prototype.rep = function(v) { return v; };
-MapHandler.prototype.stringRep = function(v) { return null; };
+handlers.MapHandler = function(){};
+handlers.MapHandler.prototype.tag = function(v) { return "map"; };
+handlers.MapHandler.prototype.rep = function(v) { return v; };
+handlers.MapHandler.prototype.stringRep = function(v) { return null; };
 
 /**
  * @constructor
  */
-var VerboseDateHandler = function(){};
-VerboseDateHandler.prototype.tag = function(v) { return "t"; };
-VerboseDateHandler.prototype.rep = function(v) {
+handlers.VerboseDateHandler = function(){};
+handlers.VerboseDateHandler.prototype.tag = function(v) { return "t"; };
+handlers.VerboseDateHandler.prototype.rep = function(v) {
     return v.getUTCFullYear()+"-"+handlers.padZeros(v.getUTCMonth()+1,2)+"-"+
            handlers.padZeros(v.getUTCDate(),2)+"T"+handlers.padZeros(v.getUTCHours(),2)+":"+
            handlers.padZeros(v.getUTCMinutes(),2)+":"+handlers.padZeros(v.getUTCSeconds(),2)+"."+
            handlers.padZeros(v.getUTCMilliseconds(),3)+"Z";
 };
-VerboseDateHandler.prototype.stringRep = function(v, h) {
+handlers.VerboseDateHandler.prototype.stringRep = function(v, h) {
     return h.rep(v);
 };
 
 /**
  * @constructor
  */
-var DateHandler = function(){};
-DateHandler.prototype.tag = function(v) { return "m"; };
-DateHandler.prototype.rep = function(v) { return v.valueOf(); };
-DateHandler.prototype.stringRep = function(v) { return v.valueOf().toString(); };
-DateHandler.prototype.getVerboseHandler = function(v) {
-    return new VerboseDateHandler();
+handlers.DateHandler = function(){};
+handlers.DateHandler.prototype.tag = function(v) { return "m"; };
+handlers.DateHandler.prototype.rep = function(v) { return v.valueOf(); };
+handlers.DateHandler.prototype.stringRep = function(v) { return v.valueOf().toString(); };
+handlers.DateHandler.prototype.getVerboseHandler = function(v) {
+    return new handlers.VerboseDateHandler();
 };
 
 /**
  * @constructor
  */
-var BinaryHandler = function(){};
-BinaryHandler.prototype.tag = function(v) { return "b"; };
-BinaryHandler.prototype.rep = function(v) { return v.str; };
-BinaryHandler.prototype.stringRep = function(v) { return v.str; };
+handlers.BinaryHandler = function(){};
+handlers.BinaryHandler.prototype.tag = function(v) { return "b"; };
+handlers.BinaryHandler.prototype.rep = function(v) { return v.str; };
+handlers.BinaryHandler.prototype.stringRep = function(v) { return v.str; };
 
 /**
  * @constructor
  */
-var UUIDHandler = function(){};
-UUIDHandler.prototype.tag = function(v) { return "u"; };
-UUIDHandler.prototype.rep = function(v) { return v.toString(); };
-UUIDHandler.prototype.stringRep = function(v) { return v.toString(); };
+handlers.UUIDHandler = function(){};
+handlers.UUIDHandler.prototype.tag = function(v) { return "u"; };
+handlers.UUIDHandler.prototype.rep = function(v) { return v.toString(); };
+handlers.UUIDHandler.prototype.stringRep = function(v) { return v.toString(); };
 
 /**
  * @constructor
  */
-var URIHandler = function(){};
-URIHandler.prototype.tag = function(v) { return "r"; };
-URIHandler.prototype.rep = function(v) { return v.uri; };
-URIHandler.prototype.stringRep = function(v) { return v.uri; };
+handlers.URIHandler = function(){};
+handlers.URIHandler.prototype.tag = function(v) { return "r"; };
+handlers.URIHandler.prototype.rep = function(v) { return v.uri; };
+handlers.URIHandler.prototype.stringRep = function(v) { return v.uri; };
 
 /**
  * @constructor
  */
-var KeywordHandler = function(){};
-KeywordHandler.prototype.tag = function(v) { return ":"; };
-KeywordHandler.prototype.rep = function(v) { return v.name; }; // NOTE: should be fqn
-KeywordHandler.prototype.stringRep = function(v, h) { return h.rep(v); };
+handlers.KeywordHandler = function(){};
+handlers.KeywordHandler.prototype.tag = function(v) { return ":"; };
+handlers.KeywordHandler.prototype.rep = function(v) { return v.name; }; // NOTE: should be fqn
+handlers.KeywordHandler.prototype.stringRep = function(v, h) { return h.rep(v); };
 
 /**
  * @constructor
  */
-var SymbolHandler = function(){};
-SymbolHandler.prototype.tag = function(v) { return "$"; };
-SymbolHandler.prototype.rep = function(v) { return v.name; }; // NOTE: should be str
-SymbolHandler.prototype.stringRep = function(v, h) { return h.rep(v); };
+handlers.SymbolHandler = function(){};
+handlers.SymbolHandler.prototype.tag = function(v) { return "$"; };
+handlers.SymbolHandler.prototype.rep = function(v) { return v.name; }; // NOTE: should be str
+handlers.SymbolHandler.prototype.stringRep = function(v, h) { return h.rep(v); };
 
 /**
  * @constructor
  */
-var QuoteHandler = function(){};
-QuoteHandler.prototype.tag = function(v) { return "'"; };
-QuoteHandler.prototype.rep = function(v) { return v.obj; };
-QuoteHandler.prototype.stringRep = function(v, h) { return null; };
+handlers.QuoteHandler = function(){};
+handlers.QuoteHandler.prototype.tag = function(v) { return "'"; };
+handlers.QuoteHandler.prototype.rep = function(v) { return v.obj; };
+handlers.QuoteHandler.prototype.stringRep = function(v, h) { return null; };
 
 /**
  * @constructor
  */
-var TaggedHandler = function(){};
-TaggedHandler.prototype.tag = function(v) { return v.tag; };
-TaggedHandler.prototype.rep = function(v) { return v.rep; };
-TaggedHandler.prototype.stringRep = function(v, h) { return null; };
+handlers.TaggedHandler = function(){};
+handlers.TaggedHandler.prototype.tag = function(v) { return v.tag; };
+handlers.TaggedHandler.prototype.rep = function(v) { return v.rep; };
+handlers.TaggedHandler.prototype.stringRep = function(v, h) { return null; };
 
 /**
  * @constructor
  */
-var TransitSetHandler = function(){};
-TransitSetHandler.prototype.tag = function(v) { return "set"; };
-TransitSetHandler.prototype.rep = function(v) {
+handlers.TransitSetHandler = function(){};
+handlers.TransitSetHandler.prototype.tag = function(v) { return "set"; };
+handlers.TransitSetHandler.prototype.rep = function(v) {
     var arr = [];
     v.forEach(function(key, set) {
         arr.push(key);
     });
     return types.taggedValue("array", arr);
 };
-TransitSetHandler.prototype.stringRep = function(v, h) { return null; };
+handlers.TransitSetHandler.prototype.stringRep = function(v, h) { return null; };
 
 /**
  * @constructor
  */
-var TransitMapHandler = function(){};
-TransitMapHandler.prototype.tag = function(v) { return "map"; };
-TransitMapHandler.prototype.rep = function(v) { return v; };
-TransitMapHandler.prototype.stringRep = function(v, h) { return null; };
+handlers.TransitMapHandler = function(){};
+handlers.TransitMapHandler.prototype.tag = function(v) { return "map"; };
+handlers.TransitMapHandler.prototype.rep = function(v) { return v; };
+handlers.TransitMapHandler.prototype.stringRep = function(v, h) { return null; };
 
 /**
  * @constructor
  */
-var ListHandler = function(){};
-ListHandler.prototype.tag = function(v) { return "list"; };
-ListHandler.prototype.rep = function(v) { return types.taggedValue("array", v.arr); };
-ListHandler.prototype.stringRep = function(b) { return null; };
+handlers.ListHandler = function(){};
+handlers.ListHandler.prototype.tag = function(v) { return "list"; };
+handlers.ListHandler.prototype.rep = function(v) { return types.taggedValue("array", v.arr); };
+handlers.ListHandler.prototype.stringRep = function(b) { return null; };
     
 handlers.defaultHandlers = function(hs) {
-    hs.set(null, new NilHandler());
-    hs.set(String, new StringHandler());
-    hs.set(Number, new NumberHandler());
-    hs.set(Long, new IntegerHandler());
-    hs.set(types.BigInteger, new BigIntegerHandler());
-    hs.set(types.BigDecimal, new BigDecimalHandler());
-    hs.set(Boolean, new BooleanHandler());
-    hs.set(Array, new ArrayHandler());
-    hs.set(Object, new MapHandler());
-    hs.set(Date, new DateHandler());
-    hs.set(types.Binary, new BinaryHandler());
-    hs.set(types.UUID, new UUIDHandler());
-    hs.set(types.URI, new URIHandler());
-    hs.set(types.Keyword, new KeywordHandler());
-    hs.set(types.Symbol, new SymbolHandler());
-    hs.set(types.Quote, new QuoteHandler());
-    hs.set(types.TaggedValue, new TaggedHandler());
-    hs.set(types.TransitSet, new TransitSetHandler());
-    hs.set(types.TransitMap, new TransitMapHandler());
-    hs.set(types.List, new ListHandler());
+    hs.set(null, new handlers.NilHandler());
+    hs.set(String, new handlers.StringHandler());
+    hs.set(Number, new handlers.NumberHandler());
+    hs.set(Long, new handlers.IntegerHandler());
+    hs.set(types.BigInteger, new handlers.BigIntegerHandler());
+    hs.set(types.BigDecimal, new handlers.BigDecimalHandler());
+    hs.set(Boolean, new handlers.BooleanHandler());
+    hs.set(Array, new handlers.ArrayHandler());
+    hs.set(Object, new handlers.MapHandler());
+    hs.set(Date, new handlers.DateHandler());
+    hs.set(types.Binary, new handlers.BinaryHandler());
+    hs.set(types.UUID, new handlers.UUIDHandler());
+    hs.set(types.URI, new handlers.URIHandler());
+    hs.set(types.Keyword, new handlers.KeywordHandler());
+    hs.set(types.Symbol, new handlers.SymbolHandler());
+    hs.set(types.Quote, new handlers.QuoteHandler());
+    hs.set(types.TaggedValue, new handlers.TaggedHandler());
+    hs.set(types.TransitSet, new handlers.TransitSetHandler());
+    hs.set(types.TransitMap, new handlers.TransitMapHandler());
+    hs.set(types.List, new handlers.ListHandler());
     return hs;
 };
 
