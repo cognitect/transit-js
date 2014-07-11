51e05bf63a067e4eff38328da7ba49667e8194cd
David Nolen <dnolen@cognitect.com>
2014-07-11 14:44:17 -0400

    fix edge case found by Brenton - need to make sure ~#cmap tag gets cached, but also that the caching happens before rep.

--- test/tests.js
+++ test/tests.js
@@ -753,3 +753,12 @@ exports.testMapCornerCase = function(test) {
     test.equal(roundtrip("[\"^ \"]"), "[\"^ \"]");
     test.done();
 };
+
+exports.testMapKeyRatioCase = function(test) {
+    var r = transit.reader("json");
+
+    test.equal(roundtrip("{\"~#cmap\":[{\"~#ratio\":[\"~n1\",\"~n2\"]},{\"^\\\"\":[\"~n2\",\"~n5\"]}]}"),
+                         "{\"~#cmap\":[{\"~#ratio\":[\"~n1\",\"~n2\"]},{\"^\\\"\":[\"~n2\",\"~n5\"]}]}");
+                          
+    test.done();
+};
