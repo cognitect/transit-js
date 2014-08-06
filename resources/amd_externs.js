1cc2043e114b5c67765b03a693eb322ef30447c4
David Nolen <david.nolen@gmail.com>
2014-08-06 08:53:14 -0400

    add externs for AMD

---
+++ resources/amd_externs.js
@@ -0,0 +1,78 @@
+/**
+ * @type {Object}
+ */
+Object.value;
+/**
+ * @type {Boolean}
+ */
+Object.done;
+/**
+ * @type {Object}
+ */
+Object.rep;
+/**
+ * @type {String}
+ */
+Object.tag;
+/**
+ * @type {Object}
+ */
+Object.init;
+/**
+ * @type {Boolean}
+ */
+Object.add;
+/**
+ * @type {Object}
+ */
+Object.finalize;
+/**
+ * @type {String}
+ */
+Object.fromArray;
+var define = function() {};
+var transit = {};
+transit.reader = function(){};
+transit.writer = function(){};
+transit.makeBuilder = function(){};
+transit.makeWriteHandler = function(){};
+transit.date = function(){};
+transit.integer = function(){};
+transit.isInteger = function(){};
+transit.uuid = function(){};
+transit.isUUID = function(){};
+transit.bigInt = function(){};
+transit.isBigInt = function(){};
+transit.bigDec = function(){};
+transit.isBigDec = function(){};
+transit.keyword = function(){};
+transit.isKeyword = function(){};
+transit.symbol = function(){};
+transit.isSymbol = function(){};
+transit.binary = function(){};
+transit.isBinary = function(){};
+transit.uri = function(){};
+transit.isURI = function(){};
+transit.map = function(){};
+transit.isMap = function(){};
+transit.set = function(){};
+transit.isSet = function(){};
+transit.list = function(){};
+transit.isList = function(){};
+transit.quoted = function(){};
+transit.isQuoted = function(){};
+transit.tagged = function(){};
+transit.isTaggedValue = function(){};
+transit.link = function(){};
+transit.isLink = function(){};
+transit.hash = function(){};
+transit.hashKey = function(){};
+transit.hashMapLike = function(){};
+transit.hashArrayLike = function(){};
+transit.equals = function(){};
+transit.extendToEQ = function(){};
+transit.mapToObject = function(){};
+transit.decoder = function(){};
+transit.UUIDfromString = function(){};
+transit.randomUUID = function(){};
+transit.stringableKeys = function(){};
