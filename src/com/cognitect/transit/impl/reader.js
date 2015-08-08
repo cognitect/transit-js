a5de25d2693f91c36712e22f75becffb8217d50d
David Nolen <david.nolen@gmail.com>
2015-08-08 10:14:03 -0400

    reformat reader

--- src/com/cognitect/transit/impl/reader.js
+++ src/com/cognitect/transit/impl/reader.js
@@ -16,50 +16,50 @@ goog.provide("com.cognitect.transit.impl.reader");
 goog.require("com.cognitect.transit.impl.decoder");
 goog.require("com.cognitect.transit.caching");
 
-goog.scope(function() {
+goog.scope(function () {
 
-var reader  = com.cognitect.transit.impl.reader,
-    decoder = com.cognitect.transit.impl.decoder,
-    caching = com.cognitect.transit.caching;
+    var reader  = com.cognitect.transit.impl.reader,
+        decoder = com.cognitect.transit.impl.decoder,
+        caching = com.cognitect.transit.caching;
 
-/**
- * A JSON unmarshaller
- * @constructor
- */
-reader.JSONUnmarshaller = function(opts) {
-    this.decoder = new decoder.Decoder(opts);
-};
+    /**
+     * A JSON unmarshaller
+     * @constructor
+     */
+    reader.JSONUnmarshaller = function (opts) {
+        this.decoder = new decoder.Decoder(opts);
+    };
 
-/**
- * @param {string} str a JSON string
- * @param {caching.ReadCache} cache a read cache
- * @returns {*}
- */
-reader.JSONUnmarshaller.prototype.unmarshal = function(str, cache) {
-    return this.decoder.decode(JSON.parse(str), cache);
-};
+    /**
+     * @param {string} str a JSON string
+     * @param {caching.ReadCache} cache a read cache
+     * @returns {*}
+     */
+    reader.JSONUnmarshaller.prototype.unmarshal = function (str, cache) {
+        return this.decoder.decode(JSON.parse(str), cache);
+    };
 
-/**
- * A transit reader
- * @constructor
- * @param {reader.JSONUnmarshaller} unmarshaller
- * @param {Object} options
- */
-reader.Reader = function(unmarshaller, options) {
-    this.unmarshaller = unmarshaller;
-    this.options = options || {};
-    this.cache = this.options["cache"] ? this.options["cache"] : new caching.ReadCache();
-};
+    /**
+     * A transit reader
+     * @constructor
+     * @param {reader.JSONUnmarshaller} unmarshaller
+     * @param {Object} options
+     */
+    reader.Reader = function (unmarshaller, options) {
+        this.unmarshaller = unmarshaller;
+        this.options = options || {};
+        this.cache = this.options["cache"] ? this.options["cache"] : new caching.ReadCache();
+    };
 
-/**
- * @param {string} str a string to be read
- * @returns {Object}
- */
-reader.Reader.prototype.read = function(str) {
-    var ret = this.unmarshaller.unmarshal(str, this.cache)
-    this.cache.clear();
-    return ret;
-};
-reader.Reader.prototype["read"] = reader.Reader.prototype.read;
+    /**
+     * @param {string} str a string to be read
+     * @returns {*}
+     */
+    reader.Reader.prototype.read = function (str) {
+        var ret = this.unmarshaller.unmarshal(str, this.cache)
+        this.cache.clear();
+        return ret;
+    };
+    reader.Reader.prototype["read"] = reader.Reader.prototype.read;
 
 });
