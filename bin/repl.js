e2bb50d1bafe85d674b9963a7586bcc3cd9ae096
David Nolen <david.nolen@gmail.com>
2021-04-26 10:42:17 -0400

    fix Node.js REPL script helper

--- bin/repl.js
+++ bin/repl.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node --harmony
 require("repl").start({useGlobal:true});
 
+global.CLOSURE_NO_DEPS = true;
 require("../deps/closure-library/closure/goog/bootstrap/nodejs.js");
 require("../deps/closure-library/closure/goog/transit_deps.js");
 
